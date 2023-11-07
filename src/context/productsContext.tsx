5c38dcf3c26f3b9fd632e0b82ac9d72e345ba4fe
Ery <ery10rx@outlook.com>
2023-11-07 20:58:45 -0300

    More performative context

--- src/context/productsContext.tsx
+++ src/context/productsContext.tsx
@@ -1,6 +1,5 @@
 import React, { createContext, ReactNode, useState, useCallback, useEffect } from "react";
 import { ItemProps } from "../../mock/menu";
-import { menuProducts } from "../../mock/menu";
 
 const MIN_QUANTITY = 1;
 const MAX_QUANTITY = 9;
@@ -22,37 +21,47 @@ export const ProductsProvider = ({ children }: { children: ReactNode }) => {
 
   const addToCart = useCallback(
     (product: ItemProps) => {
-      // Encontre o item no carrinho (se existir)
-      const existingItem = cart.find((item) => item.title === product.title);
-
-      if (existingItem) {
-        if (existingItem.quantity < MAX_QUANTITY) {
-          existingItem.quantity += 1;
-          setTotalQuantity((prevTotal) => prevTotal + 1);
+      setCart((prevCart) => {
+        const existingItem = prevCart.find((item) => item.title === product.title);
+  
+        if (existingItem) {
+          if (existingItem.quantity < MAX_QUANTITY) {
+            existingItem.quantity += 1;
+          }
+          return [...prevCart];
+        } else {
+          if (product.quantity < MAX_QUANTITY) {
+            product.quantity += 1;
+            return [...prevCart, product];
+          }
         }
-      } else {
-        if (product.quantity < MAX_QUANTITY) {
-          product.quantity += 1;
-          setCart((prevCart) => [...prevCart, product]);
-          setTotalQuantity((prevTotal) => prevTotal + 1);
-        }
-      }
+  
+        return prevCart;
+      });
+  
+      setTotalQuantity((prevTotal) => prevTotal + 1);
     },
-    [cart]
+    []
   );
-
+  
   const removeFromCart = useCallback(
     (product: ItemProps) => {
-      const existingItem = cart.find((item) => item.title === product.title);
-
-      if (existingItem) {
-        if (existingItem.quantity > MIN_QUANTITY) {
-          existingItem.quantity -= 1;
-          setTotalQuantity((prevTotal) => prevTotal - 1);
+      setCart((prevCart) => {
+        const existingItem = prevCart.find((item) => item.title === product.title);
+  
+        if (existingItem) {
+          if (existingItem.quantity > MIN_QUANTITY) {
+            existingItem.quantity -= 1;
+          }
+          return [...prevCart];
         }
-      }
+  
+        return prevCart;
+      });
+  
+      setTotalQuantity((prevTotal) => prevTotal - 1);
     },
-    [cart]
+    []
   );
 
   return (
