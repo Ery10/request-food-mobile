5314e9af045d63c729f5b036880ed6ec03b9f45b
Ery <ery10rx@outlook.com>
2023-11-14 23:31:28 -0300

    Add setSelectedProduct

--- app/(tabs)/request.tsx
+++ app/(tabs)/request.tsx
@@ -1,85 +1,29 @@
-import { useState } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ProductsContext } from '../../src/context/productsContext';
 
-export default function TabTwoScreen() {
-  // // Estado para armazenar as mesas e seus pedidos
-  // const [mesas, setMesas] = useState([
-  //   {
-  //     numeroMesa: 1,
-  //     pedidos: [],
-  //   },
-  //   {
-  //     numeroMesa: 2,
-  //     pedidos: [],
-  //   },
-  //   // Adicione mais mesas conforme necessário
-  // ]);
+const TabTwoScreen = () => {
+  const { selectedProduct } = useContext(ProductsContext) || { selectedProduct: null };
 
-  // // Função para adicionar um pedido a uma mesa
-  // const adicionarPedido = (numeroMesa, novoPedido) => {
-  //   const novasMesas = [...mesas];
-  //   const mesaIndex = novasMesas.findIndex((m) => m.numeroMesa === numeroMesa);
-  //   if (mesaIndex !== -1) {
-  //     novasMesas[mesaIndex].pedidos.push(novoPedido);
-  //     setMesas(novasMesas);
-  //   }
-  // };
-
-  // // Função para remover um pedido de uma mesa
-  // const removerPedido = (numeroMesa, indicePedido) => {
-  //   const novasMesas = [...mesas];
-  //   const mesaIndex = novasMesas.findIndex((m) => m.numeroMesa === numeroMesa);
-  //   if (mesaIndex !== -1) {
-  //     novasMesas[mesaIndex].pedidos.splice(indicePedido, 1);
-  //     setMesas(novasMesas);
-  //   }
-  // };
-
-  // return (
-  //   <ScrollView>
-  //     {mesas.map((mesa) => (
-  //       <View key={mesa.numeroMesa} style={{ margin: 10 }}>
-  //         <Text>Mesa {mesa.numeroMesa}</Text>
-  //         {mesa.pedidos.map((pedido, index) => (
-  //           <View key={index} style={{ backgroundColor: 'lightgray', padding: 5 }}>
-  //             <Text>Item: {pedido.item}</Text>
-  //             <Text>Quantidade: {pedido.quantidade}</Text>
-  //             <Button
-  //               title="Remover Pedido"
-  //               onPress={() => removerPedido(mesa.numeroMesa, index)}
-  //             />
-  //           </View>
-  //         ))}
-  //         <Button
-  //           title="Adicionar Pedido"
-  //           onPress={() =>
-  //             adicionarPedido(mesa.numeroMesa, {
-  //               item: 'Novo Item',
-  //               quantidade: 1,
-  //             })
-  //           }
-  //         />
-  //       </View>
-  //     ))}
-  //   </ScrollView>
-  // );
+  return (
+    <View style={styles.container}>
+      <Text>Detalhes do produto:</Text>
+      {selectedProduct && (
+        <>
+          <Text>{selectedProduct.title}</Text>
+          <Text>{`Preço: R$${selectedProduct.newPrice.toFixed(2)}`}</Text>
+        </>
+      )}
+    </View>
+  );
 };
+
+export default TabTwoScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
--- src/context/productsContext.tsx
+++ src/context/productsContext.tsx
@@ -7,9 +7,10 @@ const MAX_QUANTITY = 9;
 export interface ProductsContextType {
   cart: ItemProps[];
   totalQuantity: number;
+  selectedProduct: ItemProps | null;
+  setSelectedProduct: (product: ItemProps | null) => void;
   addToCart: (product: ItemProps) => void;
   removeFromCart: (product: ItemProps) => void;
-  initialTotalQuantity: number;
 }
 
 export const ProductsContext = createContext<ProductsContextType | undefined>(undefined);
@@ -17,28 +18,34 @@ export const ProductsContext = createContext<ProductsContextType | undefined>(un
 export const ProductsProvider = ({ children }: { children: ReactNode }) => {
   const [cart, setCart] = useState<ItemProps[]>([]);
   const [totalQuantity, setTotalQuantity] = useState(0);
-  const [initialTotalQuantity, setInitialTotalQuantity] = useState(0);
+  const [selectedProduct, setSelectedProduct] = useState<ItemProps | null>(null);
 
   const addToCart = useCallback(
-    (product: ItemProps) => {
+    (product: ItemProps, callback?: (updatedProduct: ItemProps) => void) => {
       setCart((prevCart) => {
         const existingItem = prevCart.find((item) => item.title === product.title);
-  
+
         if (existingItem) {
           if (existingItem.quantity < MAX_QUANTITY) {
             existingItem.quantity += 1;
+          }
+          existingItem.newPrice = existingItem.price * existingItem.quantity; // Calcular e armazenar newPrice
+          const updatedProduct = { ...existingItem };
+          if (callback) {
+            callback(updatedProduct);
           }
           return [...prevCart];
         } else {
           if (product.quantity < MAX_QUANTITY) {
             product.quantity += 1;
+            product.newPrice = product.price * product.quantity; // Calcular e armazenar newPrice
             return [...prevCart, product];
           }
         }
-  
+
         return prevCart;
       });
-  
+
       setTotalQuantity((prevTotal) => prevTotal + 1);
     },
     []
@@ -48,24 +55,28 @@ export const ProductsProvider = ({ children }: { children: ReactNode }) => {
     (product: ItemProps) => {
       setCart((prevCart) => {
         const existingItem = prevCart.find((item) => item.title === product.title);
-  
+
         if (existingItem) {
           if (existingItem.quantity > MIN_QUANTITY) {
             existingItem.quantity -= 1;
+            existingItem.newPrice = existingItem.price * existingItem.quantity; // Recalcular newPrice ao remover
+          } else {
+            // Remove completamente o item do carrinho se a quantidade for menor que MIN_QUANTITY
+            return prevCart.filter((item) => item.title !== product.title);
           }
           return [...prevCart];
         }
-  
+
         return prevCart;
       });
-  
+
       setTotalQuantity((prevTotal) => prevTotal - 1);
     },
     []
   );
 
   return (
-    <ProductsContext.Provider value={{ cart, totalQuantity, addToCart, removeFromCart, initialTotalQuantity }}>
+    <ProductsContext.Provider value={{ cart, totalQuantity, addToCart, selectedProduct, setSelectedProduct, removeFromCart }}>
       {children}
     </ProductsContext.Provider>
   );
